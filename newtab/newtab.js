fcb3e081a125e05e0f200cd3bb143010cedc24a0
skiprox <seanmichaelscanlan@gmail.com>
2019-11-02 21:02:09 -0400

    Newtab work, fixingg it up

--- newtab/newtab.js
+++ newtab/newtab.js
@@ -9,7 +9,8 @@ let options = {
 	maxPatternLength: 64,
 	minMatchCharLength: 1,
 	keys: [
-		"title"
+		"title",
+		"keywords"
 	]
 };
 let fuse;
@@ -19,43 +20,62 @@ class Newtab {
 		this.getResultsFromDb();
 		$('input').keyup((e) => {
 			let inputtext = $('.search-input').val();
-			this.updateResults(fuse.search(inputtext));
+			this.updateResults();
+			if ($(".search-input").val().length >= 1) {
+				this.updateResults();
+			} else {
+				this.updateResults(8);
+			}
 		});
 	}
 	getResultsFromDb() {
+		console.log('we are calling db why??');
 		fetch( 'https://dv-hacks.glitch.me/', {
 			method: 'GET'
 		}).then(data => {
 			return data.json()
 		}).then(json=> {
-			let results = json;
-			this.updateResults(results, 8);
+			// Setting results!!
+			results = json;
+			this.updateResults(8);
 			fuse = new Fuse(results, options);
 		});
 	}
-	updateResults(results, limit = null) {
+	updateResults(limit = null) {
 		$(".search-results").empty();
-		if (results.length) {
-			results.reverse();
-			results.forEach((elem, index) => {
-				if (limit) {
-					if (index >= limit) {
-						return false;
-					}
-				}
-				let hasFaviconClass = elem.faviconurl == null ? 'no-favicon' : '';
-				$('.search-results').append(`
-					<li class="result" data-keywords="${elem.keywords}">
-						<a href="${elem.url}" class="result-link">
-							<img src="${elem.faviconurl}" alt="" class="favicon ${hasFaviconClass}">
-							<span class="title">${elem.title}</span>
-						</a>
-					</li>`);
-			});
-		} else if ($(".search-input").val().length == 0) {
-			this.getResultsFromDb();
+		let reverseResults;
+		if ($(".search-input").val().length >= 1) {
+			reverseResults = giveBackReversed(fuse.search($(".search-input").val()));
+		} else {
+			reverseResults = giveBackReversed(results);
 		}
+		reverseResults.forEach((elem, index) => {
+			if (limit) {
+				if (index >= limit) {
+					return false;
+				}
+			}
+			let hasFaviconClass = elem.faviconurl == null ? 'no-favicon' : '';
+			$('.search-results').append(`
+				<li class="result" data-keywords="${elem.keywords}">
+					<a href="${elem.url}" class="result-link">
+						<img src="${elem.faviconurl}" alt="" class="favicon ${hasFaviconClass}">
+						<span class="title">${elem.title}</span>
+					</a>
+				</li>`);
+		});
 	}
+}
+
+const giveBackReversed = (arr) => {
+	let i = arr.length;
+	let count = 0;
+	let tmp = [];
+	while (i--) {
+		tmp[count] = arr[i];
+		count++;
+	}
+	return tmp;
 }
 
 new Newtab();
